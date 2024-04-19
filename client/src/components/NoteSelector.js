125f7ebc38d4da61dc01100d71acdd78df3d3775
Atesman <andrewates67@gmail.com>
2024-04-18 23:59:34 -0500

    Began work on NoteSelector component

---
+++ client/src/components/NoteSelector.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/NoteSelector.css';
+
+
+const NoteSelector = () => {
+
+	return (
+
+		<div className="note-selector">
+			harmonic analysis happens here
+		</div>
+	);
+}
+
+export default NoteSelector;
