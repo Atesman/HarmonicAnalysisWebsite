2338ceddf760b928ba0500be847cb6144177c6c7
Atesman <andrewates67@gmail.com>
2024-04-20 00:57:12 -0500

    Added initial note selector buttons

--- client/src/components/NoteSelector.js
+++ client/src/components/NoteSelector.js
@@ -7,7 +7,42 @@ const NoteSelector = () => {
 	return (
 
 		<div className="note-selector">
-			harmonic analysis happens here
+			<button>
+                1
+            </button>
+            <button>
+                b2
+            </button>
+            <button>
+                2
+            </button>
+            <button>
+                #2 / b3
+            </button>
+            <button>
+                3
+            </button>
+            <button>
+                4
+            </button>
+            <button>
+                #4 / b5
+            </button>
+            <button>
+                5
+            </button>
+            <button>
+                b6
+            </button>
+            <button>
+                6
+            </button>
+            <button>
+                b7
+            </button>
+            <button>
+                7
+            </button>
 		</div>
 	);
 }
