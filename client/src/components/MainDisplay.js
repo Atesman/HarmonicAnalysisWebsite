2fe0869f9f6a50d7adb431d498366d738101110e
Atesman <andrewates67@gmail.com>
2024-05-25 18:45:09 -0500

    scope of initial chord names

--- client/src/components/MainDisplay.js
+++ client/src/components/MainDisplay.js
@@ -3,39 +3,35 @@ import identifyChords from '../utils/IdentifyChords.js'
 import nameChords from '../utils/NameChords.js'
 import '../styles/MainDisplay.css';
 
-
-function MainDisplay({notes, tonic, sharpOrFlat}){
-
-	const [chordInfo, updateChords] = useState(new Map([
+const initialChords = new Map([
 		[0, [0, 2]],
 		[2, [3, 4]],
 		[4, [3, 4]],
 		[5, [0, 2]],
 		[7, [0, 1]],
 		[9, [3, 4]],
-		[11, [6, 7]],
-		]));
+		[11, [6, 7]]
+	]);
+
+
+function MainDisplay({notes, tonic, sharpOrFlat}){
+
+	const [chordInfo, updateChords] = useState(initialChords);
 	const [chordNames, updateChordNames] = useState([]);
+
 
 	useEffect(() => {
 		const newChordInfo = identifyChords(notes);
 		updateChords(newChordInfo);
-
-
 		const tempNames = nameChords(newChordInfo, tonic, sharpOrFlat);
 		updateChordNames(tempNames);
-
-		
-
 	}, [notes]);
+
 
 	useEffect(() => {
 		const tempNames = nameChords(chordInfo, tonic, sharpOrFlat);
 		updateChordNames(tempNames);
 	}, [tonic, sharpOrFlat]);
-
-
-
 
 
 	return(
